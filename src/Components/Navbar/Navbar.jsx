522e1057a0cbf22bf00f0721512e153273bb7097
Shivam190204 <broadsurvivor@gmail.com>
2025-08-02 20:13:01 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,41 +8,36 @@ const Navbar = () => {
   return (
     <nav className="flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20">
       <span className="text-xl font-bold tracking-wide">Welcome!</span>
+
       <ul
         className={`${
           menu ? "block" : "hidden"
         } mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
       >
         <a href="#About">
-          <li className="text-md transition-all duration-300 p-1 md:p-0">
-            About
-          </li>
+          <li className="text-md transition-all duration-300 p-1 md:p-0">About</li>
         </a>
         <a href="#Skill">
-          <li className="text-md transition-all duration-300 p-1 md:p-0">
-            Skills And Experience
-          </li>
+          <li className="text-md transition-all duration-300 p-1 md:p-0">Skills And Experience</li>
         </a>
         <a href="#Projects">
-          <li className="text-md transition-all duration-300 p-1 md:p-0">
-            Projects
-          </li>
+          <li className="text-md transition-all duration-300 p-1 md:p-0">Projects</li>
         </a>
         <a href="#Footer">
-          <li className="text-md transition-all duration-300 p-1 md:p-0">
-            Contact
-          </li>
+          <li className="text-md transition-all duration-300 p-1 md:p-0">Contact</li>
         </a>
+
         {/* My Resume Button */}
         <a
-             href="/src/assets/Shivam_Sonawane_Resume.pdf"
-
-          download
+          href="https://drive.google.com/uc?export=download&id=1htCguGMh_EJuefkw_-Xe5UalLnRiCG32"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-md bg-blue-600 hover:bg-blue-700 rounded-lg px-3 py-1 transition-all duration-300 ml-3 font-semibold"
         >
           My Resume
         </a>
       </ul>
+
       {showMenu ? (
         <RiMenu2Line
           size={30}
